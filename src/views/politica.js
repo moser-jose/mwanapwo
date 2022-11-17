c793ab3ef0708ee8617dfe791e427f399e6e1b85
Moser José <mosmmy@gmail.com>
2022-11-17 20:02:38 +0000

    polit

--- src/views/politica.js
+++ src/views/politica.js
@@ -7,19 +7,23 @@ function Privacidade() {
                 <h1>Política de Privacidade</h1>
             <p>Esta política de privacidade foi revista e actualizada em 15/10/2022</p>
             <p>
-                Esta política de privacidade descreve as varias formas que a Mwana Pwo utiliza para colecta, armazenamento
-                e proteção das suas informações pessoais.
-                Ao aceder, se inscrever ou utilizar nossos serviços, você estará concordando com a nossa política de privacidade.
+                Esta política de privacidade descreve as formas que o Aplicativo Mwana  Pwo Táxi utiliza para colectar, armazenamento
+                e proteção das informações pessoais dos utilizadores.
+                Ao se inscrever no Aplicativo Mwana  Pwo Táxi, você estará concordando com a nossa política de privacidade.
             </p>
             <p>
-                No âmbito da segurança das informações pessoais dos nossos utilizadores, somos obrigados a actualizar nossa
+                No âmbito da segurança das informações pessoais dos utilizadores, o Aplicativo Mwana  Pwo Táxi foi desenvolvido e será 
+                sempre atualizado com base na legislação em vigor  em Angola e a 
+                nível internacional em matéria de privacidade.</p>
+
+            <p>
+                Somos obrigados a actualizar nossa
                 política de privacidade periodicamente. Neste sentido, divulgaremos a versão revisada e actualizada em nosso site.
                 No entanto, os nossos utilizadores terão um aviso prévio das alterações feitas na
                 política de privacidade, por meio de um e-mail enviado por nós. Os utilizadores terão 
                 um período não superior a 30 dias para se manifestarem em relação a política de privacidade.
                 Terminado esse período e se não houver nenhuma manifestação por parte dos nossos utilizadores,
                 iremos considerar a política de privacidade 'pronta' e 'apta' para ser publicada.
-        
             </p>
         
             <p className="lista">#️⃣ 1. Sobre a colecta de Dados 🗃️</p>
@@ -36,27 +40,26 @@ function Privacidade() {
         
             <p className="lista">#️⃣ 2. Sobre a Finalidade dos Dados 🗃️</p>
 
-            <p>Todos os dados que a Mwana Pwo colecta, servem para um fim específico</p>
+            <p>Todos os dados que a Mwana Pwo Táxi colecta, servem para um fim específico</p>
 
             <p className="lista-p">#️⃣ 2.1 Finalidade dos dados </p>
 
             <ul>
-                <li> Chamar um táxi;</li>
-                <li> Usar o Serviço de Localização;</li>
-                <li> Prestar suporte aos utilizadores;</li>
+                <li>Utilização do serviço de táxi;</li>
+                <li>Prestar suporte aos utilizadores;</li>
             </ul>
 
-     
-        
             <p className="lista">#️⃣ 3.  Compartilhamento de Dados dos utilizadores 🗃️</p>
             
-            <p>A Mwana Pwo não compartilha nenhum dado dos nossos utilizadores que se cadastram pelo aplicativo. 
-                Temos em mente que essas informações são importantes e damos total liberdade aos nossos 
+            <p>A Mwana Pwo não compartilha nenhum dado dos nossos utilizadores. 
+                Temos em mente que essas informações são importantes e damos total liberdade aos  
                 utilizadores de gerirem essas informações de forma particular.  </p>
+
+                <p>Os dados pessoais dos utilizadores já mais, sob quaisquer condições, serão cedidos a terceiros no respeito da Lei 22/11 de 17 de Junho- Lei da Protecção de dados pessoais.</p>
                     
-            <p>Ao utilizar nossos serviços ou aceder ao nosso site, ✅ o utilizador concorda com a nossa política de privacidade.</p>
+            <p>Ao se inscrever no aplicativo, ✅ o utilizador concorda com a nossa política de privacidade.</p>
         
-            <p>Não use os nossos serviços caso não concorde com esta Política de Privacidade.<br /></p>
+            <p>Não se inscreva no aplicativo caso não concorde com esta Política de Privacidade.<br /></p>
             </div>
     
         </>
@@ -64,3 +67,12 @@ function Privacidade() {
 }
 
 export default Privacidade
+/* 
+
+1. O Aplicativo Mwana  Pwo táxi foi desenvolvido e será sempre atualizado com base na legislação em vigor  em Angola e a nível internacional em matéria de privacidade. 
+
+2.  Os dados pessoais dos utilizadores do Aplicativo se destinam apenas a utilização do serviço de táxi. 
+3 . Os dados pessoais dos utilizadores já mais, sob quaisquer condições, serão cedidos a terceiros no respeito da Lei 22/11 de 17 de Junho- Lei da Protecção de dados pessoais. 
+
+4. É todo caso, seremos únicos responsáveis pelo uso indevido no tratamento de dados pessoas dos usuários, se tal situação por alguma razão ter lugar.
+ */
